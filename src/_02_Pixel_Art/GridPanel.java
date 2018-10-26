27e23841ced3941d7b489200a7009f0f1def077f
aleksaradomirovic <league@WTS22.attlocal.net>
2018-10-25 19:54:12 -0700

    pixelArt(iP)
    almost… line 65 is AIDS

--- src/_02_Pixel_Art/GridPanel.java
+++ src/_02_Pixel_Art/GridPanel.java
@@ -6,7 +6,7 @@ import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
-public class GridPanel extends JPanel{
+public class GridPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 	private int windowWidth;
@@ -15,54 +15,57 @@ public class GridPanel extends JPanel{
 	private int pixelHeight;
 	private int rows;
 	private int cols;
-	
-	//1. Create a 2D array of pixels. Do not initialize it yet.
-	Pixel pix [][];
+
+	// 1. Create a 2D array of pixels. Do not initialize it yet.
+	Pixel pix[][];
 	private Color color;
-	
+
 	public GridPanel(int w, int h, int r, int c) {
 		this.windowWidth = w;
 		this.windowHeight = h;
 		this.rows = r;
 		this.cols = c;
-		
+
 		this.pixelWidth = windowWidth / cols;
 		this.pixelHeight = windowHeight / rows;
-		
+
 		color = Color.BLACK;
-		
+
 		setPreferredSize(new Dimension(windowWidth, windowHeight));
-		
-		//2. Initialize the pixel array using the rows and cols variables.
-		pix = new Pixel [rows][cols];
-		
-		//3. Iterate through the array and initialize each element to a new pixel.
+
+		// 2. Initialize the pixel array using the rows and cols variables.
+		pix = new Pixel[rows][cols];
+
+		// 3. Iterate through the array and initialize each element to a new pixel.
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < cols; j++) {
-				//
-				//
-				// PUT CODE HERE
-				//
-				//
+				pix = new Pixel[rows][cols];
 			}
 		}
-		
+
 	}
-	
+
 	public void setColor(Color c) {
 		color = c;
 	}
-	
+
 	public void clickPixel(int mouseX, int mouseY) {
-		//5. Use the mouseX and mouseY variables to change the color
-		//   of the pixel that was clicked. *HINT* Use the pixel's dimensions.
+		// 5. Use the mouseX and mouseY variables to change the color
+		// of the pixel that was clicked. *HINT* Use the pixel's dimensions.
+		pix[mouseX / pixelWidth][mouseY / pixelHeight].color = color;
 	}
-	
+
 	@Override
 	public void paintComponent(Graphics g) {
-		//4. Iterate through the array.
-		//   For every pixel in the list, fill in a rectangle using the pixel's color.
-		//   Then, use drawRect to add a grid pattern to your display.
-		
+		// 4. Iterate through the array.
+		// For every pixel in the list, fill in a rectangle using the pixel's color.
+		// Then, use drawRect to add a grid pattern to your display.
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				g.setColor(color.WHITE);
+				g.fillRect(cols, rows, pixelWidth, pixelHeight);
+				g.drawRect(rows, cols, pixelWidth, pixelHeight);
+			}
+		}
 	}
 }
